3f777782ea62c25dcba37e08d81871fa96fe04f7
saidousow105 <saidousow105@gmail.com>
2026-06-18 09:27:38 +0100

    commit_1

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Post (models.Model):
+    auteur= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titre=models.CharField(max_length = 200)
+    text= models.TextField()
+    create_date= models.DateTimeField(default= timezone.now)
+    published_date= models.DateTimeField(blank= True, null= True)
+
+    def publish(self):
+        self.published_date= timezone.now()
+        self.save()
+
+    def _str_(self):
+        return self.titre    
+
+# Create your models here.
